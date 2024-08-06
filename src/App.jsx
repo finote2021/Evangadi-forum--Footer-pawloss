d3c4733153d5e43dd0aef73aa94792992d66cfca
finote2021 <finotepawloss29@gmail.com>
2024-08-05 17:51:20 -0700

    Firts commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// App.js
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Footer from "./Footer/Footer";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <br />
+        <hr />
+        <br />
+        <br />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
